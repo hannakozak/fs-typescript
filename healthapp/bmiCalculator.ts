32c814e6d02587139ec415f9851e67ab60529fb5
hanna <hannawkozak@gmail.com>
2026-04-29 23:38:29 +0100

    5. WebBmi

--- healthapp/bmiCalculator.ts
+++ healthapp/bmiCalculator.ts
@@ -1,9 +1,9 @@
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
 	height = height / 100;
 	const bmi = weight / (height * height);
 
 	if (bmi < 18.5) return 'Underweight';
-	if (bmi >= 18.5 && bmi < 24.9) return 'Normal range';
+	if (bmi >= 18.5 && bmi < 25) return 'Normal range';
 	if (bmi >= 25 && bmi < 29.9) return 'Overweight';
 	return 'Obese';
 };
@@ -12,7 +12,6 @@ const args = process.argv.slice(2);
 
 if (args.length !== 2) {
 	console.log('Please provide the height and weight as arguments.');
-	process.exit(1);
 }
 
 const [heightArg, weightArg] = args;
--- healthapp/index.ts
+++ healthapp/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator.ts';
 const app = express();
 
 app.get('/ping', (_req, res) => {
@@ -7,6 +8,22 @@ app.get('/ping', (_req, res) => {
 
 app.get('/hello', (_req, res) => {
 	res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+	const height = Number(req.query.height);
+	const weight = Number(req.query.weight);
+
+	if (!height || !weight || isNaN(height) || isNaN(weight)) {
+		res.status(400).json({
+			error: 'malformatted parameters',
+		});
+		return;
+	}
+
+	const bmi = calculateBmi(Number(height), Number(weight));
+
+	res.json({ height: Number(height), weight: Number(weight), bmi });
 });
 
 const PORT = 3003;
